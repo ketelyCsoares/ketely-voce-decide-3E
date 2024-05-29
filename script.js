edd12b2c46c922371c5a3e6f80a0d2f047d4fc18
ketelyCsoares <165220665+ketelyCsoares@users.noreply.github.com>
2024-05-29 23:49:15 +0000

    29/ 05/24

---
+++ script.js
@@ -0,0 +1,51 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const txtoResultado = document.querySelector(".texto-resultado");
+
+const perguntas= [
+  {
+    enunciado: "Pergunta 1", 
+    alternativas: [
+      "Resposta 1",
+      "resposta 2"
+    ]
+  },  
+
+{
+  enunciado: "Pergunta 2", 
+  alternativas: [
+    "Resposta 1",
+    "resposta 2"
+  ]
+},
+
+{
+  enunciado: "Pergunta 3", 
+  alternativas: [
+    "Resposta 1",
+    "resposta 2"
+  ]
+},
+
+{
+  enunciado: "Pergunta 4", 
+  alternativas: [
+    "Resposta 1",
+    "Respota 2"
+    {
+      enunciado: "Pergunta 1", 
+      alternativas: [
+        "Resposta 1",
+        "resposta 2"
+      ]
+    },
+  
+{
+  enunciado: "Pergunta 5", 
+  alternativas: [
+    "Resposta 1",
+    "resposta 2"
+  ]
+}
